507b0b0759aac66b1235ecef87e4ca3a143ebc27
Samina Maimoon <94835246+sqm84@users.noreply.github.com>
2021-11-29 14:12:21 +0530

    1
    Co-Authored-By: Mustansir Jhaveri <mustansir.jhaveri@gmail.com>

---
+++ win.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("WELCOME")
+root.geometry('600x400')
+root.resizable(False, False)
+
+message = tk.Label(root, text ="Hello, World!", fg="blue", font=("Helvetica,14")).pack()
+message = tk.Label(root, text ="To Print click on print button", fg="blue", font=("Helvetica,14")).pack()
+
+def button1Function(): print("Hello World")
+def button2Function(): root.destroy()
+
+b1_button = ttk.Button(root, text="Print", command=button1Function)
+b1_button.pack(ipadx=5, ipady=5, expand=True)
+b2_button = ttk.Button(root, text="Exit", command=button2Function)
+b2_button.pack(ipadx=5, ipady=5, expand=True)
+
+root.mainloop()
